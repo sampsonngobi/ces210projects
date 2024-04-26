54c7b4352788d2f75065ce6dde76cd96975aa89e
dungasamsungo <sampsonngobi@gmail.com>
2024-04-26 16:57:29 -0300

    week 1 prep 2 core requirement completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your garde percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter = "";
+
+        // if (grade >= 90)
+        //     {
+        //         Console.WriteLine("You got an A!");
+        //     }
+        // else if (grade >= 80)
+        //     {
+        //         Console.WriteLine("You got a B!");
+        //     }
+        // else if (grade >= 70)
+        //     {
+        //         Console.WriteLine("You got a C!");
+        //     }
+        // else if (grade >= 60)
+        //     {
+        //         Console.WriteLine("You got a D!");
+        //     }
+        // else if (grade < 60)
+        //     {
+        //         Console.WriteLine("You got an F!");
+        //     }
+
+        if (grade >= 90)
+            {
+                letter = "an A";
+            }
+        else if (grade >= 80)
+            {
+                letter = "a B";
+            }
+        else if (grade >= 70)
+            {
+                 letter = "a C";
+            }
+        else if (grade >= 60)
+            {
+                 letter = "a D";
+            }
+        else if (grade < 60)
+            {
+                 letter = "an F";
+            }
+        Console.WriteLine ($"You got {letter} ");
+     
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! you've got this.");
+        }
+
+        
     }
 }
