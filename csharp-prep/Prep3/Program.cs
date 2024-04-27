5fce720351ee0d619eb86c0b2b8c6dc720f01bd9
dungasamsungo <sampsonngobi@gmail.com>
2024-04-27 20:01:28 -0300

    Week 1 prep 3 core requirements completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,35 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            // Console.Write("What is the magic number? ");
+            // magicNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }   
     }
 }
