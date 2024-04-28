4988bf8d8b2265f306902e8e7c6671615f69a413
dungasamsungo <sampsonngobi@gmail.com>
2024-04-27 21:16:14 -0300

    Week 1 prep 4 core requirements completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number (Enter 0 to quit): ");
+            String userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+
+            // AddingNewEventArgs number to list expect 0
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+              
+        }
+
+        
+        
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+           
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        int LargestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {LargestNumber}");
+        
     }
 }
